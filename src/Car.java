7d3e86249a20faf9272384df9a9b99f0f04a2973
Malik Sari <Malik.Sari@outlook.com>
2021-07-28 16:27:34 +0200

    Initial commit

---
+++ src/Car.java
@@ -0,0 +1,34 @@
+
+
+
+
+public class Car {
+
+
+
+    String make = "benz";
+    String model = "amg";
+    int Baujahr = 2010;
+    String farbe = "blaue";
+    double preis = 50000.00;
+
+
+void drive(){
+    System.out.println("you drive the car");
+}
+
+
+void brake(){
+    System.out.println("you step on the brakes");
+}
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+
+    public static void main (String [] args){
+
+
+
+        Car myCar1 = new Car();
+        Car myCar2 = new Car ();
+
+
+       System.out.println(myCar1.Baujahr);
+       System.out.println(myCar1.farbe);
+
+        System.out.println();
+
+        System.out.println(myCar2.Baujahr);
+        System.out.println(myCar2.farbe);
+
+        //myCar1.drive();
+            myCar1.brake();
+
+
+
+
+
+
+
+
+    }
+
+
+}
